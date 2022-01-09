c9ab5171b9bf47b53f7ab0707d9f5f4c7103652f
Seb Carss <seb.carss@gmail.com>
2022-01-09 22:11:52 +0000

    Create function that gets from database

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from src Lambda!'),
-    };
-    return response;
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "GET /items/{id}":
+        body = await dynamo
+          .get({
+            TableName: process.env.DYNAMODB_TABLE,
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+      case "GET /items":
+        body = await dynamo.scan({ TableName: process.env.DYNAMODB_TABLE }).promise();
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
 };
